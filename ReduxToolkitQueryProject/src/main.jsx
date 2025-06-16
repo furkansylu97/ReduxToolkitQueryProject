6078293c652260496f1f854fcd8cdc5af2e79d2e
Furkan Soylu <furkansylu97@gmail.com>
2025-06-16 23:25:36 +0300

    Store was created and performed integration process into the entire project

--- ReduxToolkitQueryProject/src/main.jsx
+++ ReduxToolkitQueryProject/src/main.jsx
@@ -1,8 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'normalize.css';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "normalize.css";
+import { Provider } from "react-redux";
+import { store } from "./store/index.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-)
+  </Provider>
+);
