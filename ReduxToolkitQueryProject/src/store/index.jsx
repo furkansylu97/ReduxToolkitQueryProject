da9c42522c8fc183ae171568f0198c3eaefb0153
Furkan Soylu <furkansylu97@gmail.com>
2025-06-17 19:30:59 +0300

    Creating photosApi and Requesting to photosAPI and redux toolkit integration operations were performed

--- ReduxToolkitQueryProject/src/store/index.jsx
+++ ReduxToolkitQueryProject/src/store/index.jsx
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersApi } from "./apis/userApi";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
     reducer: {
         [usersApi.reducerPath]: usersApi.reducer,
         [albumsApi.reducerPath]: albumsApi.reducer,
+        [photosApi.reducerPath]: photosApi.reducer,
     },
     middleware:(getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(usersApi.middleware).concat(albumsApi.middleware);
+        return getDefaultMiddleware().concat(usersApi.middleware).concat(albumsApi.middleware).concat(photosApi.middleware);
     }
 })
 
@@ -18,3 +20,5 @@ setupListeners(store.dispatch);
 export {useFetchUsersQuery, useAddUserMutation, useRemoveUserMutation} from './apis/userApi'
 
 export {useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation} from './apis/albumsApi'
+
+export {useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation} from './apis/photosApi'
