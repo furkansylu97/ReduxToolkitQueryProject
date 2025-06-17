c95759f8cea94db12a0339bc3ff90779d6ccfa4a
Furkan Soylu <furkansylu97@gmail.com>
2025-06-17 16:08:30 +0300

    Delete action was performed

--- ReduxToolkitQueryProject/src/components/UsersListItem.jsx
+++ ReduxToolkitQueryProject/src/components/UsersListItem.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import ExpandablePanel from './ExpandablePanel'
 import AlbumList from './AlbumList'
 import { GoTrash } from "react-icons/go";
+import { useRemoveUserMutation} from "../store";
 
 function UsersListItem({user}) {
 
+  const [removeUser, results] = useRemoveUserMutation();
+
+  const handleClick = () => {
+    removeUser(user);
+  }
+
   const header = (
     <>
-      <button style={{ marginRight: '20px', border: 'none' }}>
+      <button style={{ marginRight: '20px', border: 'none' }} onClick={handleClick}>
         <GoTrash />
       </button>
       {user.name}
