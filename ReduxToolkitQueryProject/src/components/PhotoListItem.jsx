f3034e87611df9f4d3f94e0a581f3d849a6e3967
Furkan Soylu <furkansylu97@gmail.com>
2025-06-18 10:04:23 +0300

    Deletion and CSS edits were performed

--- ReduxToolkitQueryProject/src/components/PhotoListItem.jsx
+++ ReduxToolkitQueryProject/src/components/PhotoListItem.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import { GoTrash } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
 
-function PhotoListItem({photo}) {
+function PhotoListItem({ photo }) {
+  const [removePhoto, results] = useRemovePhotoMutation();
+
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
+
   return (
-    <div style={ {margin:'20px' }}>
+    <div
+      style={{ margin: "20px", cursor: "pointer", position: "relative" }}
+      onClick={handleRemovePhoto}
+    >
       <img src={photo.url} alt="" />
+      <div className="deletionIconsWrapper">
+        {results.isLoading ? (
+          <CircularProgress style={{ width: "20px", height: "20px" }} />
+        ) : (
+          <GoTrash />
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default PhotoListItem
+export default PhotoListItem;
